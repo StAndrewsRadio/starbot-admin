be697c5c3e3c0605e1ff8f1f41aed1d9e81d107f
Kezz101 <kieran.wallbanks@gmail.com>
2020-08-11 17:29:55 +0100

    add file logging

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pelletier/go-toml v1.8.0
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/tidwall/buntdb v1.1.2
--- main.go
+++ main.go
@@ -5,21 +5,34 @@ import (
 
 	"github.com/StAndrewsRadio/starbot-admin/cmd/program"
 	"github.com/StAndrewsRadio/starbot-admin/vars"
+	"github.com/bwmarrin/discordgo"
 	"github.com/jessevdk/go-flags"
+	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
 )
 
 var GlobalOptions struct {
-	Debug bool `short:"d" long:"debug" description:"Displays debug messages"`
+	Debug   bool   `short:"d" long:"debug" description:"Displays debug messages"`
+	LogFile string `short:"l" long:"log-file" description:"Writes output to the file in addition to stdout"`
+}
+
+func logWrapper(msgL, caller int, format string, a ...interface{}) {
+	logrus.Warnf(format, a)
 }
 
 func init() {
-	for _, arg := range os.Args {
+	for num, arg := range os.Args {
 		if arg == "-d" || arg == "--debug" {
 			logrus.SetLevel(logrus.DebugLevel)
 			logrus.Debug("Debug logging enabled!")
+		} else if arg == "-l" || arg == "--log-file" {
+			logrus.AddHook(lfshook.NewHook(os.Args[num+1], &logrus.TextFormatter{}))
+
+			logrus.Infof("Logging to \"%s\".", os.Args[num+1])
 		}
 	}
+
+	discordgo.Logger = logWrapper
 
 	logrus.Debug("Loading parser...")
 	vars.Parser = flags.NewParser(&GlobalOptions, flags.Default)
