f67e9f642868e605f3eadf631f84c2509d7ee108
Kieran Wallbanks <kieran.wallbanks@gmail.com>
2020-09-03 13:49:30 +0100

    add frame logging to log file and debug output

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path"
+	"runtime"
 
 	"github.com/StAndrewsRadio/starbot-admin/cmd/program"
 	"github.com/StAndrewsRadio/starbot-admin/vars"
@@ -16,17 +19,28 @@ var GlobalOptions struct {
 	LogFile string `short:"l" long:"log-file" description:"Writes output to the file in addition to stdout"`
 }
 
-func logWrapper(msgL, caller int, format string, a ...interface{}) {
+func logWrapper(_, _ int, format string, a ...interface{}) {
 	logrus.Warnf(format, a)
 }
 
 func init() {
+	logrus.SetReportCaller(true)
+	logrus.SetFormatter(&logrus.TextFormatter{CallerPrettyfier: func(frame *runtime.Frame) (string, string) {
+		return "", ""
+	}})
+
+	frameLogger := func(frame *runtime.Frame) (string, string) {
+		filename := path.Base(frame.File)
+		return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
+	}
+
 	for num, arg := range os.Args {
 		if arg == "-d" || arg == "--debug" {
 			logrus.SetLevel(logrus.DebugLevel)
 			logrus.Debug("Debug logging enabled!")
+			logrus.SetFormatter(&logrus.TextFormatter{CallerPrettyfier: frameLogger})
 		} else if arg == "-l" || arg == "--log-file" {
-			logrus.AddHook(lfshook.NewHook(os.Args[num+1], &logrus.TextFormatter{}))
+			logrus.AddHook(lfshook.NewHook(os.Args[num+1], &logrus.TextFormatter{CallerPrettyfier: frameLogger}))
 
 			logrus.Infof("Logging to \"%s\".", os.Args[num+1])
 		}
